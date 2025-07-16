a407cda525e4c0abdaf895a7a542f74099aec747
Ayush9545 <ayushgotefode@gmail.com>
2025-07-16 22:55:04 +0530

    Update multiplication_table.java

--- multiplication_table.java
+++ multiplication_table.java
@@ -3,6 +3,15 @@ import java.util.*;
 
 public class multiplication_table {
     public static void main(String[] args) {
-        
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int t = s.nextInt();
+        int m = 0;
+        System.out.println("Multiplication of " + t + " is:");
+
+        for (int i = 0; i <= 10; i++) {
+            m = t * i;
+            System.out.println(t + " * " + i + " = " + m);
+        }
     }
 }
