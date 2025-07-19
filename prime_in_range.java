fb87103c648d30aa753d315f621cac84b53b2f8d
Ayush9545 <ayushgotefode@gmail.com>
2025-07-19 22:27:19 +0530

    Update prime_in_range.java

--- prime_in_range.java
+++ prime_in_range.java
@@ -2,19 +2,30 @@ import java.util.*;
 
 public class prime_in_range {
 
-    public static int range(int a){
-        int j = 1;
-        for (int i = 2; i <= a; i++) {
-            if (i % 2 == 1) {
-                return i;
+    public static boolean isprime(int a){
+        if (a == 1) {
+            return true;
+        }
+        for (int i = 2; i <= a-1; i++) {
+            if (a % i== 0){
+                return false;
             }
         }
-        return j;
+        return true;
+    }
+
+    public static void range(int a){
+        for (int i = 2; i <= a; i++) {
+            if (isprime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
     }
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.println("Enter the range: ");
         int no = s.nextInt();
-        System.out.println(range(no));
+        range(no);
     }
 }
