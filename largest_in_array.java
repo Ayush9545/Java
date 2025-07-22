3f876f25861208a7b8821a07b7b2b629ed0d1bad
Ayush9545 <ayushgotefode@gmail.com>
2025-07-22 18:06:28 +0530

    Create largest_in_array.java

---
+++ largest_in_array.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class largest_in_array {
+    public static void mm(int array[]){
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+    System.out.println(max);
+    System.out.println(min);
+    }
+
+    public static void main(String[] args) {
+        int marks[] = {11, 1, 0, 100};
+        mm(marks);
+    }
+}
