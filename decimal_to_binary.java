6ca7d5fbcf45feaf97696b40d8be84509de549a4
Ayush9545 <ayushgotefode@gmail.com>
2025-07-22 18:06:21 +0530

    Create decimal_to_binary.java

---
+++ decimal_to_binary.java
@@ -0,0 +1,13 @@
+public class decimal_to_binary {
+
+    public static void dtb(int a){
+         
+        while ( > 0) {
+            no = a % 2;
+            System.out.print(no);
+        }  
+        }
+    public static void main(String[] args) {
+        dtb(7);
+    }
+}
