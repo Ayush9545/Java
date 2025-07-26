bb47518455b1f50c69d2a9bd478c029dfe2793aa
Ayush9545 <ayushgotefode@gmail.com>
2025-07-26 19:20:04 +0530

    Update decimal_to_binary.java

--- decimal_to_binary.java
+++ decimal_to_binary.java
@@ -1,13 +1,20 @@
 public class decimal_to_binary {
 
-    public static void dtb(int a){
-         
-        while ( > 0) {
-            no = a % 2;
-            System.out.print(no);
-        }  
+    public static void dec(int num){
+
+        int decimal = 0;
+        int power = 0;
+
+        while (num > 0 ) {
+            int rem = num %2;
+            decimal = decimal + (rem * (int)Math.pow(10, power));
+            power++;
+            num = num /2;
         }
+        System.out.println(decimal);
+    }
     public static void main(String[] args) {
-        dtb(7);
+        int sum = 7;
+        dec(sum);
     }
 }
