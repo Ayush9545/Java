cf41f109a4d4742a159dac6fef7e0b444577cfb2
Ayush9545 <ayushgotefode@gmail.com>
2025-08-03 11:59:20 +0530

    Update diagonal_sum_matrix.java

--- diagonal_sum_matrix.java
+++ diagonal_sum_matrix.java
@@ -1,24 +1,33 @@
 public class diagonal_sum_matrix {
 
-    public static int dia(int number[][]){
-        int sum =0;
+    public static int dia(int number[][]) {
+        int sum = 0;
+        // for (int i = 0; i < number.length; i++) {
+        //     for (int j = 0; j < number[0].length; j++) {
+        //         if (i == j) {
+        //             sum += number[i][j];
+        //         } else if (i + j == number.length - 1) {
+        //             sum += number[i][j];
+        //         }
+        //     }
+        // }
         for (int i = 0; i < number.length; i++) {
-            for (int j = 0; j < number[0].length; j++) {
-                if (i == j) {
-                    sum += number[i][j];
-                }
-                else if (i + j == number.length-1) {
-                    sum +=number[i][j];
-                }
+            // for primary diagonal
+            sum += number[i][i];
+
+            // for secondery sum
+            if (i != number.length - i - 1) {
+                sum += number[i][number.length - i - 1];
             }
         }
         return sum;
     }
+
     public static void main(String[] args) {
-        int marks[][]={{1,2,3,4},
-                        {5,6,7,8},
-                        {9,10,11,12},
-                        {13,14,15,16}};
+        int marks[][] = { { 1, 2, 3, 4 },
+                { 5, 6, 7, 8 },
+                { 9, 10, 11, 12 },
+                { 13, 14, 15, 16 } };
         System.out.println(dia(marks));
     }
 }
