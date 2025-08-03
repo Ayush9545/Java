f712d2160940154f450afcf39f66217e671284b7
Ayush9545 <ayushgotefode@gmail.com>
2025-08-03 11:59:10 +0530

    Update count_of_no_in_matrix.java

--- count_of_no_in_matrix.java
+++ count_of_no_in_matrix.java
@@ -1,22 +1,23 @@
 public class count_of_no_in_matrix {
-    public static int repeat(int number[][],int key){
+    public static int repeat(int number[][], int key) {
         int sum = 0;
 
         for (int i = 0; i < number.length; i++) {
             for (int j = 0; j < number[0].length; j++) {
-                if (number[i][j]== 7) {
+                if (number[i][j] == key) {
                     sum++;
                 }
             }
         }
         return sum;
     }
+
     public static void main(String[] args) {
         int marks[][] = { { 1, 2, 3, 4 },
                 { 45, 6, 7, 8 },
                 { 9, 7, 11, 12 },
                 { 13, 14, 7, 16 } };
-        int key = 15;
+        int key = 7;
         System.out.println(repeat(marks, key));
     }
 }
