46c04cf0c31dabac3531144949442781a220e29f
Ayush9545 <ayushgotefode@gmail.com>
2025-08-04 23:24:44 +0530

    Create Max_arraylist.java

---
+++ Max_arraylist.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Max_arraylist {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        int a = Integer.MIN_VALUE;
+        for (int i = 0; i < list.size(); i++) {
+            a = Math.max(a, list.get(i));
+        }
+        System.out.println(a);
+    }
+}
